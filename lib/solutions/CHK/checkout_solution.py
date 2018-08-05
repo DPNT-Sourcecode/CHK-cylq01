f476342edab06ab2f474732c331f20dc0c493865
sbx_user1060 <sbx_user1060@ip-10-12-69-247.eu-west-2.compute.internal>
2018-08-05 14:46:10 +0000

    Sun Aug 05 14:46:10 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,17 +1,34 @@
 import collections
-pricelist = {'A' : 50, 'B' : 30, 'C': 20, 'D': 15}
-special_offers = {'A' :(3, 150-130), "B" : (2, 60-45)}
+free_items_offer = {'E' : (2, 'B')}
+pricelist = {'A' : 50, 'B' : 30, 'C': 20, 'D': 15, 'E': 40}
+special_offers = {'A' :[(5, 250-200), (3, 150-130)], "B" : [(2, 60-45)]}
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     bought_items = collections.Counter(skus)
     price = 0
+    # Remove all free items
+    for offer in free_items_offer:
+        (amount, free_item) = free_items_offer[offer]
+        nbr_of_free_items = bought_items[offer]/amount
+        if bought_items[free_item] - nbr_of_free_items < 0:
+            bought_items[free_item] = 0
+        else:
+            bought_items[free_item] -= nbr_of_free_items
+
+    # Calculate total price
     for e in bought_items.keys():
         if e not in pricelist:
             return -1
         price += bought_items[e]*pricelist[e]
-    for offer in special_offers.keys():
-        (amount, deduct) = special_offers[offer]
-        price -= (bought_items[offer]/amount)*deduct
+
+    #Deduct any offers
+    for item in special_offers.keys():
+        nbr_of_items = bought_items[item]
+        for offer in special_offers[item]:
+            (amount, deduct) = offer
+
+            price -= (nbr_of_items/amount)*deduct
+            nbr_of_items = bought_items[item] % amount
 
     return price
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -16,6 +16,11 @@ class TestSum(unittest.TestCase):
     def test_special_offer(self):
         self.assertEqual(checkout_solution.checkout('AAABBB'), 130 + 75)
 
+    def test_multi_special_offers(self):
+        self.assertEqual(checkout_solution.checkout('AAAAAAAAA'), 200 + 130 + 50)
+
+    def test_buy_2E_get_B_free(self):
+        self.assertEqual(checkout_solution.checkout('BBBBEE'), 45+30+80)
 
 if __name__ == '__main__':
     unittest.main()
