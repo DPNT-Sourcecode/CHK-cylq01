9d2c7192d13c607431fb327c2b598aad1591c78b
sbx_user1060 <sbx_user1060@ip-10-12-207-89.eu-west-2.compute.internal>
2018-08-05 13:03:28 +0000

    Sun Aug 05 13:03:28 UTC 2018

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -7,6 +7,9 @@ class TestSum(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(sum_solution.compute(1, 2), 3)
 
+    def test_notequal(self):
+        self.assertNotEqual(sum_solution.compute(1,1), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
